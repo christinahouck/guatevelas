d4442842d14a677e0c672c2b2a8b939d74e726e2
Christina de Martínez <christina.martinez@faithlife.com>
2021-06-08 11:09:32 +0200

    added suggestions section to the product page, cleaned up the html and sass, and made the image swipers work again.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,7 +11,7 @@ function handleViewportChange() {
 }
 
 function initCarousel() {
-    var swiper = new Swiper(".thumbnail-image-swiper", {
+    var thumbnailSwiper = new Swiper(".thumbnail-image-swiper", {
         observer: true,
         observeParents: true,
         spaceBetween: 10,
@@ -27,5 +27,19 @@ function initCarousel() {
                 direction: 'vertical'
             }
         }
+    });
+    var imageSwiper = new Swiper(".main-image-swiper", {
+        observer: true,
+        observeParents: true,
+        spaceBetween: 10,
+        slidesPerView: 1,
+        direction: 'horizontal',
+        navigation: {
+            nextEl: '.main-image-swiper .swiper-button-next',
+            prevEl: '.main-image-swiper .swiper-button-prev',
+        },
+        thumbs: {
+            swiper: thumbnailSwiper,
+        },
     });
 }
